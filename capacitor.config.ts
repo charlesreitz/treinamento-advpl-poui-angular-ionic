e8ec327facf79db8f8d207322f3e977ae7803ae5
Charles Reitz <charles.reitz@totvs.com.br>
2024-09-11 08:48:31 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'treinamento-advpl-poui-angular-ionic',
+  webDir: 'www'
+};
+
+export default config;
